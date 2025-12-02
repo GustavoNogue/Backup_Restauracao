15740adca58d31c9e5cf91c7b9a3977acdba7e4e
Gustavo N. <101331353+GustavoNogue@users.noreply.github.com>
2025-12-02 18:15:38 -0300

    feat: criação da classe SerializableCommand
    Um POJO para o JSON

---
+++ SerializableCommand.java
@@ -0,0 +1,7 @@
+public class SerializableCommand {
+    public String commandType;
+    public String deviceId;
+    public Map<String,Object> params;
+    public long timestamp;
+    // construtores, getters/setters
+}
